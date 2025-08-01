02a879f0c9833e32c438d993a6e267b89e550679
FunnyAWM <1142978300@qq.com>
2025-08-01 15:36:34 +0800

    Initial commit

---
+++ src/main/java/com/funnyawm/competitionmanagesystem/CompetitionManageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.funnyawm.competitionmanagesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompetitionManageSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompetitionManageSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/funnyawm/competitionmanagesystem/CompetitionManageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funnyawm.competitionmanagesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompetitionManageSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
