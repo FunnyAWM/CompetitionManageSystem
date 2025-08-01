0daf2873a39ec095678c39e3c0008039a1bb4757
FunnyAWM <1142978300@qq.com>
2025-08-01 16:51:29 +0800

    修改依赖项及Spring AI配置以修复APPLICATION FAILED TO START问题

---
+++ src/main/java/com/funnyawm/competitionmanagesystem/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.funnyawm.competitionmanagesystem.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+}
--- src/main/java/com/funnyawm/competitionmanagesystem/pojo/Administrator.java
+++ src/main/java/com/funnyawm/competitionmanagesystem/pojo/Administrator.java
@@ -1,4 +1,10 @@
 package com.funnyawm.competitionmanagesystem.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Administrator {
+
 }
