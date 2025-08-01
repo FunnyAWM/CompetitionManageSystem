64190514d5159e585e9bd49135645169b4b6af27
FunnyAWM <1142978300@qq.com>
2025-08-01 16:53:28 +0800

    创建了Mapper-Service-Controller模板，方便新开发者学习

--- src/main/java/com/funnyawm/competitionmanagesystem/controller/UserController.java
+++ src/main/java/com/funnyawm/competitionmanagesystem/controller/UserController.java
@@ -1,9 +1,17 @@
 package com.funnyawm.competitionmanagesystem.controller;
 
+import com.funnyawm.competitionmanagesystem.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 }
